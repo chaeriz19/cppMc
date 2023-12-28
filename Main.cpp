5f34fe7d2422d916e6b9a82cc2bc3ea097c240b3
Chris Ouboter <129173682+chaeriz19@users.noreply.github.com>
2023-12-28 23:15:48 +0100

    Load shaders

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,100 @@
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+#include"libs.cpp"
 
 bool fullscreen = false;
 
+bool loadShaders(GLuint &program) 
+{
+	bool loadSuccess = true;
+	char infoLog[512];
+	GLint success;
+	
+	std::string temp = "";
+	std::string src = "";
+
+	std::ifstream in_file;
+
+	// vertex
+	in_file.open("vertex_core.glsl");
+
+	if (in_file.is_open()) {
+		while (std::getline(in_file, temp)) {
+			src += temp + "\n";
+		}
+	}
+	else 
+		printf("Error: Could not load vertex shaders, line 24");
+	
+
+	in_file.close();
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	const GLchar* vertSrc = src.c_str();
+
+	glShaderSource(vertexShader, 1, &vertSrc, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR" << "\n";
+		std::cout << infoLog;
+		loadSuccess = false;
+		printf("Error: Could not compile vertex shader, line 33");
+	}
+
+
+	temp = "";
+	src = "";
+
+	// frag
+
+	in_file.open("fragment_core.glsl");
+
+	if (in_file.is_open()) {
+		while (std::getline(in_file, temp)) {
+			src += temp + "\n";
+		}
+	}
+	else {
+		loadSuccess = false;
+		printf("Error: Could not load fragment shader, line 52");
+	}
+
+
+	in_file.close();
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	const GLchar* fragSrc = src.c_str();
+	glShaderSource(fragmentShader, 1, &fragSrc, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		loadSuccess = false;
+		printf("Error: Could not compile fragment shader, line 63");
+	}
+
+	// program
+
+
+	program = glCreateProgram();
+
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		loadSuccess = false;
+		printf("Error: Could not link program, line 84");
+	}
+	// end
+
+	glUseProgram(0);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	return loadSuccess;
+}
 
 void ToggleFullscreen(GLFWwindow* window) {
 	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
@@ -16,7 +107,7 @@ void ToggleFullscreen(GLFWwindow* window) {
 		const int windowH = 720;
 		
 		if (glfwGetWindowMonitor(window) != nullptr) {
-			glfwSetWindowMonitor(window, nullptr, 150, 150, windowW, windowH, 0);
+			glfwSetWindowMonitor(window, nullptr, 10.0f , 50.0f, windowW, windowH, 0);
 		}
 	}
 	fullscreen = !fullscreen;
@@ -62,6 +153,12 @@ int main() {
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
 	glViewport(0, 0, windowW, windowH);
+
+
+	GLuint core_program;
+	if (!loadShaders(core_program))
+		glfwTerminate();
+
 
 	while (!glfwWindowShouldClose(window))
 	{
